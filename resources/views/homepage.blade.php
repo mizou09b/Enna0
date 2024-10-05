8b70adb7997354f973c0f81dd7b16ceb63f47a64
Ramzi YK <mizoux34@gmail.com>
2024-10-05 18:32:19 +0100

    commit changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -16,8 +16,7 @@
                         et au nom de
                         l’état algérien
                     </p>
-                    <a href="a-propos.html" class="css-button-sliding-to-bottom--blue ms-4 float-end">À
-                        Propos</a>
+                    <a href="a-propos.html" class="css-button-sliding-to-bottom--blue ms-4 float-end">À Propos</a>
                 </div>
             </div>
         </div>
@@ -26,62 +25,41 @@
     <!-- Services and Activities Section -->
     <section class="services-activities">
         <div class="container">
-            <h2 class="text-center mb-4 text-white">Nos Services et Activités</h2>
+            <h2 class="text-center text-white">Nos Services et Activités</h2>
             <hr class="w-25 mx-auto" style="border-top: 2px solid #fff; opacity: 1;">
-            {{-- <div class="services-content">
-                <div class="service-item">
-                    <img class="service-image"
-                        src="https://www.enna.dz/wp-content/uploads/2021/05/service-de-la-navigation-aerienne-800x800.jpg"
-                        alt="Service de la navigation aérienne">
-                    <h5>Service de la navigation aerienne</h5>
-                </div>
-                <div class="service-item">
-                    <img class="service-image"
-                        src="https://www.enna.dz/wp-content/uploads/2021/05/Installation-et-maintenances-cns-800x800.jpg"
-                        alt="Installation et maintenances CNS">
-                    <h5>Installation et maintenances CNS</h5>
-                </div>
-                <div class="service-item">
-                    <img class="service-image"
-                        src="https://www.enna.dz/wp-content/uploads/2022/03/formation1-800x800.jpg" alt="Formation">
-                    <h5>Formation</h5>
-                </div>
-                <div class="service-item">
-                    <img class="service-image" src="https://via.placeholder.com/300" alt="Service 4">
-                    <h5>Service 4</h5>
-                </div>
-                <div class="service-item">
-                    <img class="service-image" src="https://www.enna.dz/wp-content/uploads/2021/05/calibration-3.jpg"
-                        alt="Flight inspection unit">
-                    <h5>Service 5</h5>
-                </div>
-            </div> --}}
+
+            {{-- slider d'images --}}
             <div class="slider">
                 <div class="list">
                     <div class="item" style="--position: 1 ">
-                        <img src="https://www.enna.dz/wp-content/uploads/2021/05/service-de-la-navigation-aerienne-800x800.jpg" alt="Installation et maintenances">
+                        <a href=""><img
+                                src="https://www.enna.dz/wp-content/uploads/2021/05/service-de-la-navigation-aerienne-800x800.jpg"
+                                alt="Installation et maintenances"></a>
                         <h5>Service de la navigation aerienne</h5>
                     </div>
 
                     <div class="item" style="--position: 2 ">
-                        <img src="https://www.enna.dz/wp-content/uploads/2021/05/Installation-et-maintenances-cns-800x800.jpg"
-                        alt="Installation et maintenances CNS">
-                    <h5>Installation et maintenances CNS</h5>
+                        <a href="#"> <img
+                                src="https://www.enna.dz/wp-content/uploads/2021/05/Installation-et-maintenances-cns-800x800.jpg"
+                                alt="Installation et maintenances CNS"></a>
+                        <h5>Installation et maintenances CNS</h5>
                     </div>
 
                     <div class="item" style="--position: 3 ">
-                        <img src="https://www.enna.dz/wp-content/uploads/2022/03/formation1-800x800.jpg" alt="Formation">
+                        <a href=""><img
+                                src="https://www.enna.dz/wp-content/uploads/2022/03/formation1-800x800.jpg"
+                                alt="Formation"></a>
                         <h5>Formation</h5>
                     </div>
 
                     <div class="item" style="--position: 4 ">
-                        <img src="https://via.placeholder.com/300" alt="Service 4">
+                        <a href=""> <img src="https://via.placeholder.com/300" alt="Service 4"></a>
                         <h5>Service 4</h5>
                     </div>
 
                     <div class="item" style="--position: 5">
-                        <img src="https://www.enna.dz/wp-content/uploads/2021/05/calibration-3.jpg"
-                        alt="Flight inspection unit">
+                        <a href=""><img src="https://www.enna.dz/wp-content/uploads/2021/05/calibration-3.jpg"
+                                alt="Flight inspection unit"></a>
                         <h5>Flight isnpection unit 5</h5>
                     </div>
                 </div>
@@ -91,8 +69,8 @@
     </section>
 
     {{-- data chiffre section --}}
-    <h2 class="text-center pt-4 h2-styles">L'ENNA EN CHIFFRE</h2>
-    <hr class="w-25 mx-auto" style="border-top: 2px solid #0F6CA6; opacity: 1">
+    <h2 class="text-center pt-4 h2-styles">L'Enna en chiffre</h2>
+    <hr class="w-25 mx-auto" style="border-top: 2px solid #4dabf7; opacity: 1">
     <section class="data-section">
         <div class="box-chiffre">
             <div class="boxes">
